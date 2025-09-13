2dcadc6d0316e2968e6b78716e77269b3a744d5e
Amanda J Kendal-Brown <15990675+ajkendal@users.noreply.github.com>
2025-09-12 18:18:37 -0700

    Refactor Current weather UI and data handling
    Improved the Current component by extracting and rounding weather data values, formatting the date, and updating the UI to display city, date, icon, and temperature. Enhanced layout and styling in Current.module.scss for better alignment and responsiveness, and adjusted global styles for the current daily section.

--- src/components/Current.tsx
+++ src/components/Current.tsx
@@ -14,6 +14,32 @@ const Current = ({
   searchCity,
   isMetric,
 }: CurrentProps) => {
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  }
+
+  const feelsLike = currentData
+    ? Math.round(currentData.apparent_temperature)
+    : '-'
+
+  const humidity = currentData
+    ? Math.round(currentData.relative_humidity_2m)
+    : '-'
+
+  const wind = currentData ? Math.round(currentData.wind_speed_10m) : '-'
+
+  const precipitation = currentData
+    ? Math.round(currentData.precipitation)
+    : '-'
+  const temperature = currentData ? Math.round(currentData.temperature_2m) : '-'
+
+  const date = currentData
+    ? new Date(currentData.time).toLocaleDateString('en-US', options)
+    : ''
+
   return (
     <div className={styles['current-container']}>
       {isLoading ? (
@@ -23,41 +49,47 @@ const Current = ({
         </div>
       ) : (
         <div className={`content-container ${styles['main-current']}`}>
-          text
+          <div className={styles['current-text']}>
+            <p className='text-preset-4'>{searchCity}</p>
+            <p className='text-preset-6'>{date}</p>
+          </div>
+          <div className={styles['current-icon']}>
+            <img
+              src={getWeatherIcon(currentData.weathercode)}
+              alt='Weather Icon'
+            />
+            <p className='text-preset-1'>
+              {temperature.toString() !== 'undefined' ? temperature + '°' : '-'}
+            </p>
+          </div>
         </div>
       )}
       <div className={styles['current-blocks']}>
         <div className={`content-container ${styles['current-block']}`}>
           <h4 className='text-preset-6'>Feels Like</h4>
           <p className='text-preset-3'>
-            {currentData.apparent_temperature.toString() !== 'undefined'
-              ? Math.round(currentData.apparent_temperature) + '°'
-              : '-'}
+            {feelsLike.toString() !== 'undefined' ? feelsLike + '°' : '-'}
           </p>
         </div>
         <div className={`content-container ${styles['current-block']}`}>
           <h4 className='text-preset-6'>Humidity</h4>
           <p className='text-preset-3'>
-            {currentData.relative_humidity_2m.toString() !== 'undefined'
-              ? Math.round(currentData.relative_humidity_2m) + '%'
-              : '-'}
+            {humidity.toString() !== 'undefined' ? humidity + '%' : '-'}
           </p>
         </div>
         <div className={`content-container ${styles['current-block']}`}>
           <h4 className='text-preset-6'>Wind</h4>
           <p className='text-preset-3'>
-            {currentData.wind_speed_10m.toString() !== 'undefined'
-              ? Math.round(currentData.wind_speed_10m) +
-                (isMetric ? ' km/h' : ' mph')
+            {wind.toString() !== 'undefined'
+              ? wind + (isMetric ? ' km/h' : ' mph')
               : '-'}
           </p>
         </div>
         <div className={`content-container ${styles['current-block']}`}>
           <h4 className='text-preset-6'>Precipitation</h4>
           <p className='text-preset-3'>
-            {currentData.precipitation.toString() !== 'undefined'
-              ? Math.round(currentData.precipitation) +
-                (isMetric ? ' mm' : ' in')
+            {precipitation.toString() !== 'undefined'
+              ? precipitation + (isMetric ? ' mm' : ' in')
               : '-'}
           </p>
         </div>
